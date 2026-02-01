a2db2c1cdb712ca6343fe07dd75c7b6d0b1f6dc6
Prenses123 <137319134+Prenses123@users.noreply.github.com>
2026-02-01 18:03:00 +0400

    Update 13-binary_tree_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -12,12 +12,9 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-
 	if (tree->left != NULL || tree->right != NULL)
 		count = 1;
-
 	count += binary_tree_nodes(tree->left);
 	count += binary_tree_nodes(tree->right);
-
 	return (count);
 }
