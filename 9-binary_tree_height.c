99f19b13e2c61a4a85aed66e42c08eba7f2f993e
Prenses123 <137319134+Prenses123@users.noreply.github.com>
2026-02-01 17:26:51 +0400

    Update 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,15 +14,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-
 	if (tree->left == NULL && tree->right == NULL)
 		return (0);
-
 	left_h = binary_tree_height(tree->left);
 	right_h = binary_tree_height(tree->right);
-
 	if (left_h > right_h)
 		return (1 + left_h);
-
 	return (1 + right_h);
 }
