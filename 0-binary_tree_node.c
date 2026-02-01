8db141b91c1c8f2c8d7f3e93602e57d7fdf73fad
Your Name <you@example.com>
2026-02-01 10:59:08 +0000

    add

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -15,11 +15,9 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	node = malloc(sizeof(binary_tree_t));
 	if (node == NULL)
 		return (NULL);
-
-	node->n = value;
 	node->parent = parent;
+	node->n = value;
 	node->left = NULL;
 	node->right = NULL;
-
 	return (node);
 }
